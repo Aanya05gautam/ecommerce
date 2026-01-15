09861bc5c8c65b1c6f4ec4fd96fbc8b14027d5db
Aanya05gautam <aanyagautam2006@gmail.com>
2026-01-15 16:53:50 +0530

    Allow Render host in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true, // allow all hosts
+    allowedHosts: ['ecommerce-frontend-ljqe.onrender.com'], // your Render URL
+    port: 4173 // optional, your preview port
+  }
 })
